11d1b9b8eccf7945b2562d904d57e101a177848d
DoloresM <DoloresM@users.noreply.github.com>
2023-01-08 21:56:17 -0800

    failed mongodb server attempt1

---
+++ organize-me/server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const mongoose = require("mongoose");
+const session = require("express-session");
+
+//DATABASE SETUP
+mongoose.connect(
+    "mongodb://localhost/o-m",
+    {useNewUrlParser: true}
+  );
+
+//Package documentation - https://www.npmjs.com/pakcage/connect-mongo
+const MongoStore = require("connect-mongo")(session)
+
+//Creating express application
+const app = express(); 
+
+//setting up database
+const dbString = "mongodb://localhost:27017/organize-me/";
+//options to run DB
+
+const dbOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+const connection = mongoose.createdConnection(dbString, dbOptions);
+
+//JSON MIDDLEWARE FOR PARSING REQUESTS
+app.use(express.join());
+app.use(express.urlencoded({extended:true}));
+
+
+// PROPERTIES RELATED TO THE EXPRESS SESSION MIDDLEWARE 
+const sessionStore = new MongoStore ({
+    mongooseConnection: connection,
+    collection: "sessions"
+});
+    // configuration -- express-session resource: https://www.npmjs.com/package/express-session
+    // here the session middleware is getting passed to the express instance, in which the opject 
+        // being passed represents the options
+    // Sesssion Store Implementation - represents iformation about a client/ user moving across the site - in which the databasae store the data
+app.use(session({
+    secret: "some secret",
+    resave: false,
+    saveUnitialized: true,
+    store: sessionStore,
+    cookie: {
+        maxAge: 1000 * 60 * 60 *24 // equating to equal one full day
+    }
+}));
+
+
+
+app.get("/", (req, res, next) =>{
+    res.send("<h2>live from 3000!</h2>")
+})
+
+app.listen(3001, ()=>{
+   console.log("Server running on 3001");
+});
