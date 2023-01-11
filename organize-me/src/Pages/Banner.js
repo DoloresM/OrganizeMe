73dbaee7e44ade2de0133f408e18ec07b2dd3134
DoloresM <DoloresM@users.noreply.github.com>
2023-01-11 12:19:30 -0800

    icons download and working implementation

--- organize-me/src/Pages/Banner.js
+++ organize-me/src/Pages/Banner.js
@@ -1,7 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import {faYoutube} from "@fortawesome/free-brands-svg-icons";
+
+// const element = <FontAwesomeIcon icon={faCoffee} />
+
 function Banner(){
     return(
         <div className="banner">
             <h3>How OrganizeMe Works</h3>
+            <FontAwesomeIcon icon={faCoffee}/> 
+            <FontAwesomeIcon icon={faYoutube}/> 
+            <hr></hr>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquet a mauris quis semper. Nunc nulla ligula, euismod vel eros eu, commodo ornare ipsum.</p>
         </div>
     )
