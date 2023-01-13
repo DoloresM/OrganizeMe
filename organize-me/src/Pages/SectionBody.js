a48ae5ea8aa9158e2367fa9af3f6090c84f236f7
DoloresM <DoloresM@users.noreply.github.com>
2023-01-12 20:52:47 -0800

    sections w/ image removed _ ND

--- organize-me/src/Pages/SectionBody.js
+++ organize-me/src/Pages/SectionBody.js
@@ -14,8 +14,8 @@ import  { React} from "react";
                             et quasi architecto beatae vitae dicta sunt explicabo.
                 </p>
                 </section>
-            <section>
-                    <img src="https://images.unsplash.com/photo-1622658641558-235f26dd270b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"/>
+            <section className="section-img">
+                    safsd
             </section>   
         </div>
 
