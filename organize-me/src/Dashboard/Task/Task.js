10c5224a2af1536f3a81a66924316daeb0dcf999
DoloresM <DoloresM@users.noreply.github.com>
2023-01-31 23:14:21 -0800

    task form formating draft complete

--- organize-me/src/Dashboard/Task/Task.js
+++ organize-me/src/Dashboard/Task/Task.js
@@ -7,14 +7,24 @@ const Task = ()=>{
                 <h1>Task</h1>
                 <form name="task" className="taskList">
                     <div className="task"><label for="title">Title: </label><input type="text" name="title"/></div>
-                    <div className="task"><label for="title">Start Date: </label><input type="text" name="title"/></div>
-                    <div className="task"><label for="title">Due Date: </label><input type="text" name="title"/></div>
-                    <div className="task"><label for="title">Description: </label><input type="textarea" name="title"/></div>
+                    <div className="task"><label for="title">Start Date: </label><input type="date" name="title"/></div>
+                    <div className="task"><label for="title">Due Date: </label><input type="date" name="title"/></div>
+
+                    <div  className="task"> 
+                        <span>Description</span> 
+                        <textarea id="text-box" name="text" oninput='this.style.height = "";this.style.height = this.scrollHeight + "px"'></textarea>
+                    </div>
+                    <div  className="task"> 
+                        <span>Requirements</span> 
+                        <textarea id="text-box" name="text" oninput='this.style.height = "";this.style.height = this.scrollHeight + "px"'></textarea>
+                    </div>
+
+
                     <div className="task"><label for="title">Location: </label><input type="textarea" name="title"/></div>
-                    <div className="tasks"> <label for="title">Requirements: </label><input type="text" name="title"/></div>
-                    <div className="tasks"><label for="title">Add Task: </label><input type="text" name="title"/></div>
-                    <div className="tasks"><label for="title">View List: </label><input type="text" name="title"/></div>
-                    <div className="tasks"><label for="title">Add Alert: </label><input type="text" name="title"/></div> 
+                    
+                    <div className="tasks"><button>Add Task</button></div>
+                    <div className="tasks"><button>View List</button></div>
+                    <div className="tasks"><button>Add Alert</button></div> 
                 </form>
             </div>
         </>
