77b6cb646a013d553fb42bc11f3e1399b8a10133
DoloresM <DoloresM@users.noreply.github.com>
2023-02-01 13:40:41 -0800

    dashboard forms first draft

--- organize-me/src/Dashboard/Project/Project.js
+++ organize-me/src/Dashboard/Project/Project.js
@@ -2,11 +2,37 @@
 const Project = ()=>{
     return (
         <>
-            <div><h1>Project</h1></div>
-            <form>
-                
-            </form>
-        </>
+           
+        
+
+            <div className="content">
+                <h1>Project</h1>
+                <form name="project" className="projectList">
+                    <div className="task"><label for="title">Title: </label><input type="text" name="title"/></div>
+                    <div className="task"><label for="title">Start Date: </label><input type="date" name="title"/></div>
+                    <div className="task"><label for="title">Due Date: </label><input type="date" name="title"/></div>
+
+                    <div  className="task"> 
+                        <span>Description</span> 
+                        <textarea id="text-box" name="text" oninput='this.style.height = "";this.style.height = this.scrollHeight + "px"'></textarea>
+                    </div>
+                    <div  className="task"> 
+                        <span>Requirements</span> 
+                        <textarea id="text-box" name="text" oninput='this.style.height = "";this.style.height = this.scrollHeight + "px"'></textarea>
+                    </div>
+
+
+                    <div className="task"><label for="title">Location: </label><input type="textarea" name="title"/></div>
+                    
+                    <div className="tasks"><button>Add Task</button></div>
+                    <div className="tasks"><button>View List</button></div>
+                    <div className="tasks"><button>Add Alert</button></div> 
+                </form>
+            </div>
+            </> 
     )
 }
+
+
+    
 export default  Project;
--- organize-me/src/Dashboard/Schedule/Schedule.js
+++ organize-me/src/Dashboard/Schedule/Schedule.js
@@ -1,9 +1,34 @@
 const Schedule = ()=>{
     return (
         <>
-            <div><h1>Schedule</h1></div>
+              <div className="content">
+                <h1>Project</h1>
+                <form name="schedule" className="scheduleList">
+                    <div className="task"><label for="title">Title: </label><input type="text" name="title"/></div>
+                    <div className="task"><label for="title">Start Date: </label><input type="date" name="title"/></div>
+                    <div className="task"><label for="title">Due Date: </label><input type="date" name="title"/></div>
+
+                    <div  className="task"> 
+                        <span>Description</span> 
+                        <textarea id="text-box" name="text" oninput='this.style.height = "";this.style.height = this.scrollHeight + "px"'></textarea>
+                    </div>
+                    <div  className="task"> 
+                        <span>Requirements</span> 
+                        <textarea id="text-box" name="text" oninput='this.style.height = "";this.style.height = this.scrollHeight + "px"'></textarea>
+                    </div>
+
+
+                    <div className="task"><label for="title">Location: </label><input type="textarea" name="title"/></div>
+                    
+                    <div className="tasks"><button>Add Task</button></div>
+                    <div className="tasks"><button>View List</button></div>
+                    <div className="tasks"><button>Add Alert</button></div> 
+                </form>
+            </div>
         </>
     )
 }
 
 export default  Schedule;
+
+
