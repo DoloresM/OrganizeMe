0370685c68ec934f00a97fb888383f53baf3c6b7
DoloresM <DoloresM@users.noreply.github.com>
2023-05-25 20:12:07 -0400

    blog header draft foundation

--- organize-me/src/Pages/BlogHeader.js
+++ organize-me/src/Pages/BlogHeader.js
@@ -1,10 +1,13 @@
 function BlogHeader (){
     return(
-        <div className="blog-header">
-            <div className="main-blog"></div>
-            <div className="main-blog-aside"></div>
-            <div className="main-blog-sub"> </div>
-            <div className="main-blog-sub"> </div>
+        <div className="blog-header content">
+            <div className="main-blog"><h3>main</h3></div>
+            <div className="main-blog-aside"><h3>aside</h3></div>
+            <div className="main-blog-sub"><h3>sub</h3> </div>
+            <div className="main-blog-sub"><h3>sub</h3></div>
+            <div className="main-blog-sub"><h3>sub</h3></div>
+   
+           
 
         </div>
     )
