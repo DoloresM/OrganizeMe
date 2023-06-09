13ccf2276a3df4217914830933ffe861397c83de
DoloresM <DoloresM@users.noreply.github.com>
2023-06-08 22:36:05 -0400

    managing sign in form draft 0

--- organize-me/src/Pages/SignIn/SignIn.js
+++ organize-me/src/Pages/SignIn/SignIn.js
@@ -7,36 +7,42 @@ import Footer from '../MainHome/Footer/Footer.js';
 
 const SignIn = ()=>{
 
-const [profile, setProfile] = useState({});
+const [profile, setProfile] = useState([]);
 
 
 const handleProfile = (event)=>{
-  let name = event.target.name;
-  let value = event.target.value
-  profile[name] = value;
-  setProfile(profile)
+  // let name = event.target.name;
+  // let value = event.target.value
+  // profile[name] = value;
+  // setProfile(profile)
+  console.log(event.target.value)
+  console.log(event.target.name)
   };
 
 //CAPTURE VALUE & RETURN COMPONENT
 const handleSubmit = (event)=>{
   event.preventDefault()
- console.log( ...event.currentTarget);
+//  console.log( ...event.currentTarget);
+// console.log(event.target.value)
+//  console.log(event)
 }
+
+
 
 
     return(
         <>
           <Nav/>
           <h1>Create Your Account! </h1>
-            <form  onSubmit={handleSubmit} className="signIn "action="" methhod="POST" >
-              <div> <label for="firstName">First:<input id="one" type="text" name="firstName" onBlur={handleProfile}/> </label></div>
-              <div> <label for="lastName">Last:<input id="two" type="text" name="lastName" onBlur={handleProfile} /></label> </div>
-              <div><label for="userName"> Username: <input type="text" name="userName" onBlur={handleProfile}/></label></div>
-              <div><label for="email">Email:  <input type="email" name="email" onBlur={handleProfile} /> </label></div>
-              <div><label for="email"> Confirm Email: <input type="email" name="email" onBlur={handleProfile}  /> </label></div>
+            <form  onSubmit={e => {handleSubmit(e)}} className="signIn "action="" methhod="POST" >
+              <div> <label for="firstName">First:<input id="one" type="text" name="firstName" onChange={handleProfile}/> </label></div>
+              <div> <label for="lastName">Last:<input id="two" type="text" name="lastName" onChange={handleProfile} /></label> </div>
+              <div><label for="userName"> Username: <input type="text" name="userName" onChange={handleProfile}/></label></div>
+              <div><label for="email">Email <input type="email" name="email" onChange={handleProfile} /> </label></div>
+              <div><label for="email"> Confirm Email: <input type="email" name="email" onChange={handleProfile}  /> </label></div>
               <div><label for="password">Password:<input type="password" name="password"/></label></div>
                 
-                  
+              
                   
                 
               
