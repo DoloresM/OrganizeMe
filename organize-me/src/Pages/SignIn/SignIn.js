9d8e7d06d47ec5402202d22deea8af5ea9b6c0fb
DoloresM <DoloresM@users.noreply.github.com>
2023-06-20 20:07:44 -0400

    attempting email validation

--- organize-me/src/Pages/SignIn/SignIn.js
+++ organize-me/src/Pages/SignIn/SignIn.js
@@ -17,41 +17,50 @@ const handleProfile = (event)=>{
     // update the profile object w/ event.target "name:value" as properties
     [name]:value
   })) 
-   };
+};
 
   console.log(profile)
+
+  const emailConfirmation = ()=>{
+    return profile.email === profile.email2 ? console.log("yes"): console.log("no")
+  }
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    console.log(e);
+    emailConfirmation()
+    }
+    
+   
+  
 
 
     return(
         <>
           <Nav/>
           <h1>Create Your Account! </h1>
-            <form   className="signIn "action="" methhod="POST" >
-              <div> <label for="firstName">First:<input id="one" type="text" name="firstName" onChange={handleProfile}/> </label></div>
-              <div> <label for="lastName">Last:<input id="two" type="text" name="lastName" onChange={handleProfile} /></label> </div>
-              <div><label for="userName"> Username: <input type="text" name="userName" onChange={handleProfile}/></label></div>
-              <div><label for="email">Email <input type="email" name="email" onChange={handleProfile} /> </label></div>
-              <div><label for="email"> Confirm Email: <input type="email" name="email" onChange={handleProfile}  /> </label></div>
-              <div><label for="password">Password:<input type="password" name="password"/></label></div>
+            <form  onSubmit={handleSubmit} className="signIn "action="" methhod="POST" >
+              <div> <label for="firstName">First:<input id="one" type="text" name="firstName" onChange={handleProfile} required/> </label></div>
+              <div> <label for="lastName">Last:<input id="two" type="text" name="lastName" onChange={handleProfile} required /></label> </div>
+              <div><label for="userName"> Username: <input type="text" name="userName" onChange={handleProfile} required /></label></div>
+              <div><label for="email">Email <input type="email" name="email" placeholder="Enter a valid email address" onChange={handleProfile} required/> </label></div>
+              <div><label for="email"> Confirm Email: <input type="email" name="email2"  onChange={handleProfile} required /> </label></div>
+              <div><label for="password">Password:<input type="password" name="password" required/></label></div>
                 
-              
-                  
-                
-              
               <div>
                 <label for="confirmPassword">
                 confirm passoword:
-                <input type="password" name="confirmPassword"  />
+                <input type="password" name="confirmPassword" required />
                 </label>
               </div>
               <div>
                 <label for="birthDate">
                 Date of Birth:
-                {/* <input type="date" name="birthDate" onBlur={handleProfile}/> */}
+                <input type="date" name="birthDate" onBlur={handleProfile}/>
                 </label>
               </div>
               <div>
-                <input type="submit" value="create account"  />
+                <button type="submit">Sign Up</button>
               </div>
           </form>
           <button><Link className="form-button" to="/Dashboard/Home">View Dashboard</Link></button>
